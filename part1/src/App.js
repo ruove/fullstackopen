13601bdf6544399dc47907e6208ea85947b3019b
Vesa Ruohonen <vesa.ruohonen@gmail.com>
2023-04-18 19:10:44 +0300

    create project

---
+++ part1/src/App.js
@@ -0,0 +1,35 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const footer = () => {
+  return (
+    <div>
+      footer testing by
+      <a href="https://github.com/ruove">ruove</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App;

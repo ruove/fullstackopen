d8020fa15b97092edc80e129a52727ecd8420f49
Vesa Ruohonen <vesa.ruohonen@gmail.com>
2023-04-20 21:29:19 +0300

    did 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,21 +4,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [sum, setSum] = useState(0)
 
   const giveGood = () => {
     setGood(good + 1)
-    setTotal(total + 1)
+    setCounter(counter + 1)
+    setSum(sum + 1)
     console.log('clicked good')
   }
   const giveNeutral = () => {
     setNeutral(neutral + 1)
-    setTotal(total + 1)
+    setCounter(counter + 1)
+    console.log('clicked neutral')
   }
   const giveBad = () => {
     setBad(bad + 1)
-    setTotal(total + 1)
+    setCounter(counter + 1)
+    setSum(sum - 1)
+    console.log('clicked bad')
   }
+
   return (
     <div>
       <h1>Anna palautetta</h1>
@@ -36,20 +42,40 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
-        total={total}
-      />
+        counter={counter}/>
+      <Average
+        sum={sum}
+        counter={counter}/>
+      <Positives 
+        good={good}
+        counter={counter}/>
     </div>
   )
 }
 
-const Stats = ({ good, neutral, bad, total }) => {
+const Stats = ({ good, neutral, bad, counter }) => {
   return (
     <div>
       <p>Hyviä {good}</p>
       <p>Neutraaleita {neutral}</p>
       <p>Huonoja {bad}</p>
-      <p>Total {total}</p>
+      <p>Kaikki {counter}</p>
     </div>
+  )
+}
+
+const Average = ({ sum, counter }) => {
+  if (counter === 0) {
+    return <p>Ei palautteita</p>
+  }
+  return (
+    <p>Keskiarvo {sum / counter}</p>
+  )
+}
+
+const Positives = ({ good, counter }) => {
+  return (
+    <p>Positiiviset {good / counter * 100} %</p>
   )
 }
 

b0098775d8932d221d344f6dede06d5dd8dcf805
Vesa Ruohonen <vesa.ruohonen@gmail.com>
2023-04-21 09:38:38 +0300

    done up to 1.12

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,30 +4,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [counter, setCounter] = useState(0)
+  const [total, setCounter] = useState(0)
   const [sum, setSum] = useState(0)
 
   const giveGood = () => {
     setGood(good + 1)
-    setCounter(counter + 1)
+    setCounter(total + 1)
     setSum(sum + 1)
     console.log('clicked good')
   }
   const giveNeutral = () => {
     setNeutral(neutral + 1)
-    setCounter(counter + 1)
+    setCounter(total + 1)
     console.log('clicked neutral')
   }
   const giveBad = () => {
     setBad(bad + 1)
-    setCounter(counter + 1)
+    setCounter(total + 1)
     setSum(sum - 1)
     console.log('clicked bad')
   }
 
   return (
     <div>
-      <h1>Anna palautetta</h1>
+      <h1>Let's have feedback</h1>
       <Button
         handleClick={giveGood}
         text="Good" />
@@ -37,46 +37,64 @@ const App = () => {
       <Button
         handleClick={giveBad}
         text="Bad" />
-      <h1>Tilastot</h1>
+      <h1>Statistics</h1>
       <Stats
         good={good}
         neutral={neutral}
         bad={bad}
-        counter={counter}
+        total={total}
         sum={sum} />
     </div>
   )
 }
 
-const Stats = ({ good, neutral, bad, counter, sum }) => {
+const Stats = ({ good, neutral, bad, total, sum }) => {
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback yet</p>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <p>Hyviä {good}</p>
-      <p>Neutraaleita {neutral}</p>
-      <p>Huonoja {bad}</p>
-      <p>Kaikki {counter}</p>
-      <Average
-        sum={sum}
-        counter={counter}/>
-      <Positives 
-        good={good}
-        counter={counter}/>
+      <table>
+      <StatisticsLine text="Good" value={good} />
+      <StatisticsLine text="Neutral" value={neutral} />
+      <StatisticsLine text="Bad" value={bad} />
+      <StatisticsLine text="Total" value={total} />
+      <StatisticsLine text="Average" value={<Average sum={sum} total={total} />} />
+      <StatisticsLine text="Positives" value={<Positives good={good} total={total} />} />
+      </table>
     </div>
   )
 }
 
-const Average = ({ sum, counter }) => {
-  if (counter === 0) {
-    return <p>Ei palautteita</p>
-  }
+const StatisticsLine = ({text, value}) => {
   return (
-    <p>Keskiarvo {sum / counter}</p>
+    <tr>     
+      <td>{text}:</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
-const Positives = ({ good, counter }) => {
+const Average = ({ sum, total }) => {
+  if (total === 0) {
+    return <p>Ei palautteita</p>
+  }
   return (
-    <p>Positiiviset {good / counter * 100} %</p>
+    <>{(sum / total).toFixed(2)}</>
+  )
+}
+
+const Positives = ({ good, total }) => {
+  if (total === 0) {
+    return <p>Ei palautteita</p>
+  }
+  return (
+    <>{(good / total * 100).toFixed(2)} %</>
   )
 }
 
